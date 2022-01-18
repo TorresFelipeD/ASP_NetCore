e0a4ae3ff0639cbf83cfda7b83dd9ce67be08723
Diego Chaparro <torresfeliped@gmail.com>
2022-01-17 20:47:06 -0500

    se vuelve a crear la app

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,8 @@ namespace ASP_NetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
