c432551b7c4d3e2468dfeb2130f66994c3d7ea85
Diego Chaparro <torresfeliped@gmail.com>
2022-01-31 22:42:30 -0500

    Se crea una nueva vista parcial

--- Controllers/AsignaturaController .cs
+++ Controllers/AsignaturaController .cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using ASP_NetCore.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +15,16 @@ namespace ASP_NetCore.Controllers
             };
 
             return View(asignatura);
+        }
+
+        public IActionResult MultiAsignatura(){
+            var listAsignatura = new List<Asignatura>(){
+                new Asignatura(){Nombre = "Programación"},
+                new Asignatura(){Nombre = "Algebra"},
+                new Asignatura(){Nombre = "Historia"},
+                new Asignatura(){Nombre = "Ciencias"}
+            };
+            return View(listAsignatura);
         }
     }
 }
